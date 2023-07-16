7513a7a2752557e799792cbeb98656199884ed1e
Aryan Dua <cs5200475@iitd.ac.in>
2023-07-16 17:50:24 +0530

    Labyrinth

--- CSES/Graph Algorithms/Labyrinth.cpp
+++ CSES/Graph Algorithms/Labyrinth.cpp
@@ -28,6 +28,9 @@ using namespace std;
 const ll N = 2e5 + 4;
 const ll mod = 1e9 + 7;
 vs grid;
+vvl prevCoord (1000, vl (1000, -1));
+vvl vis (1000, vl (1000, 0));
+
 
 ll mul(ll a, ll b){
     return (a*b)%mod;
@@ -302,9 +305,85 @@ bool findInSet(sl s, ll x){
         return false;
 }
 
+void bfs(pl coord, ll m, ll n){
+    queue<pl> q;
+    q.push(coord);
+    vl di = {1, -1, 0, 0};
+    vl dj = {0, 0, 1, -1};
+    while (!q.empty()){
+        pl p = q.front();
+        q.pop();
+        if(grid[p.f][p.s]=='B')
+            return;
+        for0(k, 4){
+            ll row = p.f+di[k];
+            ll col = p.s+dj[k];
+            bool cond1 = row<m && col<n && row>=0 && col>=0;
+            bool cond2 = grid[row][col]=='.' || grid[row][col]=='B';
+            if(cond1 && cond2 && !vis[row][col]){
+                q.push(make_pair(row, col));
+                prevCoord[row][col] = k;
+                vis[row][col] = 1;
+            }
+        }   
+    }
+    return;
+}
+
 void solve()
 {
-    ll n = input_n();      
+    ll m = input_n();
+    ll n = input_n();
+    for0(i, m)
+        grid.push_back(input_string());
+    string path;
+    pl end;
+    pl start;
+    // make pair a standard function to make a pair of the given types
+    for0(i, m)
+        for0(j, n)
+        {
+            if(grid[i][j]=='A')
+                start = make_pair(i, j);
+            if(grid[i][j]=='B')
+                end = make_pair(i, j);
+        }
+    bfs(start, m, n);
+    // prevCoord stores the direction it took to reach the i, j
+    while(end!=start){
+        ll dir = prevCoord[end.first][end.second];
+        if(dir==0){
+            end.first = end.first-1;
+            path+="D";
+        }
+        else if(dir==1){
+            end.first = end.first+1;
+            path+="U";
+        }
+        else if(dir==2){
+            end.second = end.second-1;
+            path+="R";
+        }
+        else if(dir==3){
+            end.second = end.second+1;
+            path+="L";
+        }  
+        else{
+            path = "";
+            break;
+        } 
+    }
+    // the path will be in reverse order
+    reverse(path.begin(), path.end());
+    ll sz = path.size();
+    if(sz==0){
+        print("NO");
+    }  
+    else{
+        print("YES");
+        print(sz);
+        print(path);
+    }         
 }
 
 int main(int argc, char *argv[]) {
